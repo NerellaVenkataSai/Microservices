9a8c9975dd8fe000cd437704c049ce2771cb4d72
NerellaVenkataSai <84185688+NerellaVenkataSai@users.noreply.github.com>
2021-10-14 09:49:50 +0530

    full sample app in working condition
    updated ingress-srv.yaml to support regex in routes
    
    nginx.ingress.kubernetes.io/use-regex: "true"

--- sample app/moderation/index.js
+++ sample app/moderation/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', async (req, res) => {
-  console.log("Event Received:", req.body.type);
+  console.log("Event Received moderation:", req.body.type);
   const { type, data } = req.body;
 
   if (type === 'CommentCreated') {
