481e4403b3d7f5ea1671918d5d9151e002cfa862
NerellaVenkataSai <84185688+NerellaVenkataSai@users.noreply.github.com>
2021-10-17 14:49:57 +0530

    Error handle middleware basic approach
    will resolve error handler middleware with generic approach to handle all kinds of errors with standard errors in future commits

--- BigTicketSystems/Ticketing/auth/src/index.ts
+++ BigTicketSystems/Ticketing/auth/src/index.ts
@@ -3,7 +3,9 @@ import {json} from 'body-parser';
 import {currentUserRouter} from './routes/current-user';
 import {signInRouter} from './routes/signin';
 import {signOutRouter} from './routes/signout';
-import {signUpRouter} from './routes/signup'
+import {signUpRouter} from './routes/signup';
+import { errorHandler } from "./middlewares/error-handler";
+
 
 const app = express();
 app.use(json());
@@ -13,6 +15,7 @@ app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!!!!')
