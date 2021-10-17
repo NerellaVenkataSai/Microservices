e00a2bfeb22d65c0856fcb48839cbb0247913dae
NerellaVenkataSai <84185688+NerellaVenkataSai@users.noreply.github.com>
2021-10-17 15:52:23 +0530

    Subclasses for Error
    create subclasses for parent class Error.
    To differentiate the type of error
    
    Note: standard error format image 12

--- BigTicketSystems/Ticketing/auth/src/middlewares/error-handler.ts
+++ BigTicketSystems/Ticketing/auth/src/middlewares/error-handler.ts
@@ -1,4 +1,12 @@
+/**
+ * Define error-handling middleware functions in the same way as other middleware functions, except error-handling functions have four arguments instead of three: (err, req, res, next)
+ * 
+ * while processing api if service throws the error it will come to error handler and follow the logic inside error handler
+ */
+
 import { Request, Response, NextFunction } from "express";
+import {RequestValidationError} from '../errors/request-validation-error';
+import {DatabaseConnectionError} from '../errors/database-connection-error';
 
 export const errorHandler = (
   err: Error,
@@ -6,7 +14,13 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log("Something went wrong", err);
+  if(err instanceof RequestValidationError) {
+    console.log("RequestValidationError", err);
+  }
+
+  if(err instanceof DatabaseConnectionError) {
+    console.log("DatabaseConnectionError", err);
+  }
 
   res.status(400).send({
     message: err.message,
